c12943e058ebe5421295cf248cca802de82c590a
javierub <javierub38@gmail.com>
2025-07-20 10:19:08 +0200

    Rank_01 initiation

---
+++ Rank_01/Libft/ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jurbanej <jurbanej@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 10:34:36 by jurbanej          #+#    #+#             */
+/*   Updated: 2025/04/29 10:34:37 by jurbanej         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	size_t	len;
+	size_t	i;
+	char	*ptr;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = f(i, s[i]);
+		i++;
+	}
+	ptr[len] = '\0';
+	return (ptr);
+}
